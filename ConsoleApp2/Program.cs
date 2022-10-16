06387fe9388ac916f026960eabcafda4ace0d099
XRJoker <Dana.lune@yandex.ru>
2022-10-16 19:41:37 +0300

    Practice1_OS

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем каталог для файла
+            string path = @"D:\Документы\Гаврилова БББО-07-21\os1\workfile";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Введите строку для записи в файл:");
+            string text = Console.ReadLine();
+
+            // запись в файл
+            using (FileStream fstream = new FileStream($"{path}\\work2.txt", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+
+            // чтение из файла
+            using (FileStream fstream = File.OpenRead($"{path}\\work2.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+
+            Console.ReadLine();
+
+            string pathf = $"{path}\\work2.txt";
+            FileInfo fileInf = new FileInfo(pathf);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine($"Вы хотите удалить файл? 0 - нет 1 - да");
+                string ansdel = Console.ReadLine();
+                if (ansdel=="0")
+                {
+                    Console.WriteLine($"Ок.");
+                }
+
+                if (ansdel == "1")
+                {
+                    fileInf.Delete();
+                    Console.WriteLine($"Файл удален.");
+                }
+
+                if (!(ansdel == "1") && !(ansdel == "0"))
+                {
+                    fileInf.Delete();
+                    Console.WriteLine($"Error.");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+namespace HelloApp
+{
+    
+class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string pathf = @"D:\Документы\Гаврилова БББО-07-21\os1\workfile";
+            DirectoryInfo dirInfo = new DirectoryInfo(pathf);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Введите имя: ");
+            string na = Console.ReadLine();
+            Console.WriteLine("Укажите возраст: ");
+            string ag = Console.ReadLine();
+            // сохранение данных
+            using (FileStream fs = new FileStream(@"D:\Документы\Гаврилова БББО-07-21\os1\workfile\wfile_3.json", FileMode.OpenOrCreate))
+            {
+                Person poke = new Person()
+                {
+                    Name = na,
+                    Age = ag
+                };
+                await JsonSerializer.SerializeAsync<Person>(fs, poke);
+                Console.WriteLine("Данные сохранены в файл.");
+            }
+
+            // чтение данных
+            using (FileStream fs = new FileStream(@"D:\Документы\Гаврилова БББО-07-21\os1\workfile\wfile_3.json", FileMode.OpenOrCreate))
+            {
+                Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
+                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+            }
+
+            string pathmf = @"D:\Документы\Гаврилова БББО-07-21\os1\workfile\wfile_3.json";
+            FileInfo fileInf = new FileInfo(pathmf);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine($"Вы хотите удалить файл? 0 - нет 1 - да");
+                string ansdel = Console.ReadLine();
+                if (ansdel == "0")
+                {
+                    Console.WriteLine($"Ок.");
+                }
+
+                if (ansdel == "1")
+                {
+                    fileInf.Delete();
+                    Console.WriteLine($"Файл удален.");
+                }
+
+                if (!(ansdel == "1") && !(ansdel == "0"))
+                {
+                    fileInf.Delete();
+                    Console.WriteLine($"Error.");
+                }
+            }
+
+        }
+    }
+}
